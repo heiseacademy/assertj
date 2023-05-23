9c7162f46afb97768c99735c1f0e52b208811846
Sandra Parsick <mail@sandra-parsick.de>
2023-05-23 08:37:43 +0200

    assertj fluent api sample

--- 02-fluent-api/src/test/java/com/github/sparsick/heise/assertj/AssertJTest.java
+++ 02-fluent-api/src/test/java/com/github/sparsick/heise/assertj/AssertJTest.java
@@ -1,10 +1,25 @@
 package com.github.sparsick.heise.assertj;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class AssertJTest {
 
     @Test
-    void hamcrestSample(){
+    void hamcrestSample() {
+        MatcherAssert.assertThat("Hello World",
+                Matchers.allOf(Matchers.equalTo("Hello World"), Matchers.containsString("Hello")));
+    }
+
+    @Test
+    void assertJSample(){
+        assertThat("Hello World")
+                .isEqualTo("Hello World")
+                .contains("Hello");
     }
 }
