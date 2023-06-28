6c130c7de0a00bf4474618989e7db7f049e25ec6
Sandra Parsick <mail@sandra-parsick.de>
2023-06-28 07:53:56 +0200

    init description assertion

---
+++ 06-description-assertion/src/test/java/com/github/sparsick/heise/assertj/AssertJTest.java
@@ -0,0 +1,29 @@
+package com.github.sparsick.heise.assertj;
+
+import org.junit.jupiter.api.Test;
+
+
+public class AssertJTest {
+
+
+    @Test
+    void description() {
+
+    }
+
+    @Test
+    void errorMessage() {
+
+    }
+
+    @Test
+    void lazyErrorMessage() {
+    }
+
+
+
+
+
+
+
+}
