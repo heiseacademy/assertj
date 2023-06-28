4134460b5ab530a04cf66e38a6067ff356cc4a28
Sandra Parsick <mail@sandra-parsick.de>
2023-06-28 08:11:17 +0200

    sample description assertion

--- 06-description-assertion/src/test/java/com/github/sparsick/heise/assertj/AssertJTest.java
+++ 06-description-assertion/src/test/java/com/github/sparsick/heise/assertj/AssertJTest.java
@@ -2,22 +2,30 @@ package com.github.sparsick.heise.assertj;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class AssertJTest {
 
 
     @Test
     void description() {
-
+        String message = "Hello World";
+        assertThat(message).as("check message").isNull();
     }
 
     @Test
     void errorMessage() {
-
+        String message = "Hello World";
+//        assertThat(message).overridingErrorMessage("message is not null").isNull();
+        assertThat(message).withFailMessage("message is not null").isNull();
     }
 
     @Test
     void lazyErrorMessage() {
+        String message = "Hello World";
+//        assertThat(message).overridingErrorMessage(() -> "message is not null").isNull();
+        assertThat(message).withFailMessage(() -> "message is not null").isNull();
     }
 
 
